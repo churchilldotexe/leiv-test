e81ee071dfda017aa374b52de5fe541dc867144d
churchilldotexe <churchillexe@gmail.com>
2025-10-25 09:12:50 +0800

    init commit

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig, passthroughImageService } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from '@astrojs/sitemap';
+
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  site:"https://leivaassistants.netlify.app/",
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [sitemap(), icon()],
+  image:{
+    service: passthroughImageService()
+  }
+});
